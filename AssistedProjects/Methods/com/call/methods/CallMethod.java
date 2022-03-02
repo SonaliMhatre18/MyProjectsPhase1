0226369571ed927db88d77f8336689dc8f7194f2
Sonali Bhanudas Mhatre <sonalimhatre367@gmail.com>
2022-03-02 19:34:28 +0530

    adding

---
+++ AssistedProjects/Methods/com/call/methods/CallMethod.java
@@ -0,0 +1,17 @@
+package com.call.methods;
+public class CallMethod {
+
+int val=250;
+
+int operation(int val) {
+	val =val*10/100;
+	return(val);
+}
+
+public static void main(String args[]) {
+	CallMethod d = new CallMethod();
+	System.out.println("Before operation value of data is "+d.val);
+	d.operation(100);
+	System.out.println("After operation value of data is "+d.val);
+	}
+}
