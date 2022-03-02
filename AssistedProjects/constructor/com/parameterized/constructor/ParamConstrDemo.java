62c5dd70f3fc40a9945b97b81d4702fda582708f
Sonali Bhanudas Mhatre <sonalimhatre367@gmail.com>
2022-03-03 00:26:38 +0530

    adding

---
+++ AssistedProjects/constructor/com/parameterized/constructor/ParamConstrDemo.java
@@ -0,0 +1,26 @@
+package com.parameterized.constructor;
+
+class Std{
+	int id;
+	String name;
+
+	Std(int i,String n)
+	{
+	id=i;
+	name=n;
+	}
+
+	void display() {
+	System.out.println(id+" "+name);
+	}
+}
+
+public class ParamConstrDemo {
+public static void main(String[] args) {
+
+	Std std1=new Std(2,"Sonali");
+	Std std2=new Std(10,"Mansi");
+	std1.display();
+	std2.display();
+		}
+}
