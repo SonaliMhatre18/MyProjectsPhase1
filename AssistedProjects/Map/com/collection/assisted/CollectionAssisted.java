c7b231b99b9436667cd7d30051fad18ad8b1b1af
Sonali Bhanudas Mhatre <sonalimhatre367@gmail.com>
2022-03-03 01:14:16 +0530

    adding collection

---
+++ AssistedProjects/Map/com/collection/assisted/CollectionAssisted.java
@@ -0,0 +1,55 @@
+package com.collection.assisted;
+
+
+import java.util.*;
+
+public class CollectionAssisted {
+
+	public static void main(String[] args) {
+		//creating arraylist
+		System.out.println("ArrayList");
+		ArrayList<String> city=new ArrayList<String>();   
+	      city.add("Mumbai");//
+	      city.add("Pune");    	   
+	      System.out.println(city);  
+		
+		//creating vector
+	      System.out.println("\n");
+	      System.out.println("Vector");
+	      Vector<Integer> vec = new Vector();
+	      vec.addElement(25); 
+	      vec.addElement(40); 
+	      System.out.println(vec);
+		
+		//creating linkedlist
+	      System.out.println("\n");
+	      System.out.println("LinkedList");
+	      LinkedList<String> names=new LinkedList<String>();  
+	      names.add("Rohan");  
+	      names.add("Rahul");  	      
+	      Iterator<String> itr=names.iterator();  
+	      while(itr.hasNext()){  
+	       System.out.println(itr.next());  
+	       
+	       //creating hashset
+	       System.out.println("\n");
+	       System.out.println("HashSet");
+	       HashSet<Integer> set=new HashSet<Integer>();  
+	       set.add(201);  
+	       set.add(203);  
+	       set.add(202);
+	       set.add(204);
+	       System.out.println(set);
+	       
+	       //creating linkedhashset
+	       System.out.println("\n");
+	       System.out.println("LinkedHashSet");
+	       LinkedHashSet<Integer> set2=new LinkedHashSet<Integer>();  
+	       set2.add(31);  
+	       set2.add(33);  
+	       set2.add(32);
+	       set2.add(34);	        
+	       System.out.println(set2);
+	      	} 
+	      }  
+	}
