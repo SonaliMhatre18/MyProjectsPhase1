2056c85d5fabb77df0c524b80e8d40985d0ee948
Sonali Bhanudas Mhatre <sonalimhatre367@gmail.com>
2022-03-07 09:50:39 +0530

    adding final project

---
+++ Final Project/com/locker/company/Locker.java
@@ -0,0 +1,11 @@
+package com.locker.company;
+
+public class Locker {
+	
+	public static void main(String[] args) {
+		System.out.println("WELCOME TO LOCKER APPLICATION \n" + " Sonali Mhatre ");
+		WelcomeScreen welcomeScreen = new WelcomeScreen();
+		welcomeScreen.display();
+	}
+
+}
