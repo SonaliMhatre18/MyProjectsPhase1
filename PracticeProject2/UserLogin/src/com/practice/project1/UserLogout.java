4119ad19529148e1a27f809d5b75961040c8f37e
Sonali Bhanudas Mhatre <sonalimhatre367@gmail.com>
2022-03-19 16:08:14 +0530

    adding User login Practice Project

---
+++ PracticeProject2/UserLogin/src/com/practice/project1/UserLogout.java
@@ -0,0 +1,40 @@
+package com.practice.project1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/UserLogout")
+public class UserLogout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+  
+    public UserLogout() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		session.invalidate();
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.println("Logged out of session.<br>");
+		out.println("</body></html>");
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
